352d403f039ae22a9d894dddfa2afc0d11302480
manithlakvidu2002 <lakvidu00@gmail.com>
2023-08-19 10:12:37 +0530

    New Update in GetVsLoad

---
+++ src/lk/ijse/gdse/orm/hibernate/GetVsLoad.java
@@ -0,0 +1,44 @@
+package lk.ijse.gdse.orm.hibernate;
+
+import lk.ijse.gdse.orm.hibernate.config.SessionFactoryConfig;
+import lk.ijse.gdse.orm.hibernate.entity.Customer;
+import org.hibernate.Session;
+
+public class GetVsLoad {
+    public static void main(String[] args) {
+
+        Session session = SessionFactoryConfig
+                .getInstance().getSession();
+        int cusId = (int) session.save(getCustomer());
+        session.close();
+
+        // 1. Get
+        Session session2 = SessionFactoryConfig
+                .getInstance().getSession();
+        System.out.println("------------Get--------------");
+        Customer get = session2.get(Customer.class, 2);
+        System.out.println("Customer Id of Get: " +get.getId());
+        System.out.println("Customer Name of Get: " +get.getName());
+        session2.close();
+
+        // 2. Load
+        Session session3 = SessionFactoryConfig
+                .getInstance().getSession();
+        System.out.println("----------Load-------------");
+        Customer load = session3.load(Customer.class, 2);
+        System.out.println("Customer Id of Load: " +load.getId());
+        System.out.println("Customer Name of Load: " +load.getName());
+
+        session3.close();
+    }
+
+
+    private static Customer getCustomer() {
+        Customer customer = new Customer();
+        customer.setName("Saman");
+        customer.setAddress("Galle");
+        customer.setSalary(25000.00);
+        customer.setAge(20);
+        return customer;
+    }
+}
